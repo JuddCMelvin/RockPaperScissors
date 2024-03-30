c49526d6b8a8ec91c50edf420ebd6cb5e5770b90
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-03-30 10:49:58 -0400

    initial code from course homework for regular rockpaperscissor

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+let userSelection = prompt("rock, paper or scissors?"); 
+let computerSelection = Math.random();                                                          
+
+//conditional logic goes here
+if (computerSelection < 0.34) {
+	computerSelection = "rock";
+} else if(computerSelection <= 0.67) {
+	computerSelection = "paper";
+} else {
+	computerSelection = "scissors";
+}
+
+//compare the userSeletion and the computerSelection to determine the winner
+let compare=function(selection1,selection2){
+    if(selection1===selection2){
+        return "The result is a tie!";
+    }
+    else if(selection1==="rock"){
+        if(selection2==="scissors"){
+            return "rock wins"
+        }
+        else if(selection2==="paper"){
+            return "paper wins";
+        }
+    }
+    else if(selection1==="paper"){
+        if(selection2==="scissors"){
+            return "scissors win"
+        }
+        else if(selection2==="rock"){
+            return "paper wins";
+        }
+    }
+    else if(selection1==="scissors"){
+        if(selection2==="paper"){
+            return "scissors win"
+        }
+        else if(selection2==="rock"){
+            return "rock wins";
+        }
+    }    
+}
+console.log(compare(userSelection,computerSelection));
